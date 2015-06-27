3cf2d6a131fb212d44b42d83af530c8860538b32
oab00 <oab93@hotmail.com>
2015-06-27 03:33:07 +0300

    basic functionality populating bars

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,6 +1,23 @@
 'use strict';
 
 angular.module('nightlifeApp')
-.controller('MainCtrl', function ($scope) {
+.controller('MainCtrl', function ($scope, $http) {
+
+	$scope.input_location = 'montreal';
 	
+	$scope.search = function() {
+		if ($scope.input_location) {
+			// remove everything except letters, spaces and dashes
+			$scope.input_location = $scope.input_location.replace(/[^\w -]?[\d]?/g, '');
+
+			$http.get('/api/bars/search/' + $scope.input_location)
+				.success(function(data) {
+					$scope.bars = data.message.businesses;
+					console.log(data.message.businesses);
+				});
+		}
+	};
+
+
+
 });
